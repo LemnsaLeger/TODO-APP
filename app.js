15f9f4b7a98b0d3f0b2ad7782cdfd83b0a72f27b
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-04-08 03:49:32 -0700

    Update, Delete todo From DOM, Delete from LocalStorage.

--- app.js
+++ app.js
@@ -3,93 +3,108 @@ const moonIcon = document.querySelector(".icon-moon");
 const sunIcon = document.querySelector(".icon-sun");
 const inputCheckBox = document.querySelector(".input-checkbox");
 const inputCheck = document.querySelector(".inputcheckbox");
-const todoContainer = document.querySelector("#container .todos-container");
-const todoCrossIcon = todoContainer.querySelector("li .icon-cross");
+const todoContainer = document.querySelector("#container > ul");
+let todoCrossIcon = document.querySelectorAll(".icon-cross");
 const todoCheckbox = document.querySelector(".checkbox");
 const todoCheck = document.querySelector(".todo");
 
+let todosArray = []; // let: for modifying the array(reasign)
 
-const todosArray = [];
+function CreateTodoObject(todo, status = "pending") {
+  this.todo = todo;
+  this.status = status;
 
-function CreateTodoObject(todo, status = 'pending') {
- this.todo = todo;
- this.status = status;
-
- return { todo, status }
+  return { todo, status };
 }
 
 // function to add todo
 function getTodo() {
-    if(!inputField.value) {
-        return;
-    } else {
-            let input;
-            input = inputField.value.trim();
-            const todoObject = new CreateTodoObject(input);
-            todosArray.push(todoObject);
+  if (!inputField.value) {
+    return;
+  } else {
+    let input;
+    input = inputField.value.trim();
+    const todoObject = new CreateTodoObject(input);
+    todosArray.push(todoObject);
 
-            saveTodoToLocalStorage(todoObject);
-            return input;
-    }
+    saveTodoToLocalStorage(todoObject);
+    return input;
+  }
 }
 
-function todoLiItem(todoText='') {
-    let li = document.createElement("li");
-    let div = document.createElement("div"); // for checkbox
-    div.className = "input checkbox";
+function todoLiItem(todoText = "") {
+  let li = document.createElement("li");
+  li.className = "todo-item";
+  let div = document.createElement("div"); // for checkbox
+  div.className = "input checkbox";
 
-    let img = document.createElement("img");
-    img.className = "todo checkbox";
-    img.src = "./images/icon-check.svg";
-    img.alt = "icon-check";
+  let img = document.createElement("img");
+  img.className = "todo checkbox";
+  img.src = "./images/icon-check.svg";
+  img.alt = "icon-check";
 
-    let todo = document.createElement("span");
-    if(todoText === '') {
-      todo.textContent = getTodo(); // receive todo text from user
-    } else {
-        todo.textContent = todoText;
-    }
+  let todo = document.createElement("span");
+  if (todoText === "") {
+    todo.textContent = getTodo(); // receive todo text from user
+  } else {
+    todo.textContent = todoText;
+  }
 
-    let deleteIcon = document.createElement("img");
-    deleteIcon.className = "icon-cross";
-    deleteIcon.src = "./images/icon-cross.svg";
-    deleteIcon.alt = "icon-cross";
+  let deleteIcon = document.createElement("img");
+  deleteIcon.className = "icon-cross";
+  deleteIcon.src = "./images/icon-cross.svg";
+  deleteIcon.alt = "icon-cross";
 
-    li.appendChild(div);
-    li.appendChild(img);
-    li.appendChild(todo);
-    li.appendChild(deleteIcon);
+  li.appendChild(div);
+  li.appendChild(img);
+  li.appendChild(todo);
+  li.appendChild(deleteIcon);
 
-    return li;
+  return li;
 }
 
 // add todo to DOM on button click(checkbox)
 inputCheckBox.addEventListener("click", () => {
-    let li;
+  let li;
 
-    li = todoLiItem();
-    if(li) {
-            todoContainer.prepend(li);
-    }
+  li = todoLiItem();
+  if (li) {
+    todoContainer.prepend(li);
+    deleteTodo(li);
+  }
 });
-
 
 // save todos in local storage
 function saveTodoToLocalStorage() {
-    localStorage.setItem("todos", JSON.stringify(todosArray));
+  localStorage.setItem("todos", JSON.stringify(todosArray));
 }
 
 // fetch todos from localstorage and display to the DOM
 document.addEventListener("DOMContentLoaded", () => {
   let todos = [];
   todos = JSON.parse(localStorage.getItem("todos"));
-
-    let li;
-    todos.forEach((todo, index) => {
-        if(todo.todo){
-            li = todoLiItem(todo.todo);
-            todoContainer.prepend(li);
-        }
-    });
+  todosArray.push(...todos);
+  let li;
+  todos.forEach((todo, index) => {
+    if (todo.todo) {
+      li = todoLiItem(todo.todo);
+      todoContainer.prepend(li);
+      deleteTodo(li);
+    }
+  });
 });
 
+// function to delete a todo
+const deleteTodo = (todo) => {
+  let deleteIcon = todo.querySelector(".icon-cross");
+  let todoText = todo.querySelector("span").textContent;
+  todosArray = JSON.parse(localStorage.getItem("todos")) || [];
+
+  let index = todosArray.findIndex((t) => t.todo === todoText);
+  deleteIcon.addEventListener("click", () => {
+    todo.remove(); // remove the todo from dom
+    // Remove the todo from the todos array in localStorage
+    todosArray.splice(index, 1); // Modify the array in place
+    saveTodoToLocalStorage(); // Update localStorage
+  });
+};
